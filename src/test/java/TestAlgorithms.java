6a61727c3a3ee3d1eede9e01d69a1302f353c762
Vaggelis Mavropoulos <e.mavropoulos@pamediakopes.gr>
2019-04-16 16:24:13 +0300

    Add Tests for Algorithm class

--- src/test/java/TestAlgorithms.java
+++ src/test/java/TestAlgorithms.java
@@ -1,18 +1,34 @@
+import hackerrank.Algorithms;
 import hackerrank.Warmup;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class TestAlgorithms {
 
     @Test
-    public void gradingStudents(){
-
+    public void gradingStudents() {
+        int[] arr = new int[] { 73, 67, 38, 33};
+        int[] result = Algorithms.gradingStudents(arr);
+        Assert.assertEquals(new int[]{75, 67, 40 ,33}, result);
     }
 
     @Test
-    public void birthdayCakeCandles(){
-        int[] arr = new int[] { 3, 2, 1, 3 };
-        int result = Warmup.birthdayCakeCandles(arr);
-        Assert.assertEquals(2, result);
+    public void divisibleSumPairs() {
+        int n = 6;
+        int k = 3;
+        int[] array = new int[]{1, 3, 2, 6, 1, 2};
+        int result = Algorithms.divisibleSumPairs(n, k, array);
+        Assert.assertEquals(5, result);
     }
+
+    @Test
+    public void extraLongFactorials() {
+        int n = 25;
+        BigInteger bigInteger = Algorithms.extraLongFactorials(n);
+        Assert.assertEquals(new BigInteger("15511210043330985984000000"), bigInteger);
+    }
+
 }
