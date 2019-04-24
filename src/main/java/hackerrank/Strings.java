fc287b78151e8000a0a546be1ddabc4e1df84b6f
Vaggelis Mavropoulos <e.mavropoulos@pamediakopes.gr>
2019-04-24 16:45:25 +0300

    Solve String problem hackerrank

--- src/main/java/hackerrank/Strings.java
+++ src/main/java/hackerrank/Strings.java
@@ -34,6 +34,17 @@ public class Strings {
         else{
             System.out.println("No");
         }
+    }
 
+    /**
+     * https://www.hackerrank.com/challenges/java-strings-introduction/problem
+     */
+    public static void lexicographically(){
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length()+B.length());
+        System.out.println((A.compareTo(B) > 0)?"Yes":"No");
+        System.out.println(A.substring(0,1).toUpperCase().concat(A.substring(1))+" "+B.substring(0,1).toUpperCase().concat(B.substring(1)));
     }
 }
