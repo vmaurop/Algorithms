9e2a7fae520d2699b2c428649c0b671bda50f909
Vaggelis Mavropoulos <vmaurop@gmail.com>
2019-11-02 20:38:46 +0200

    Adding hacker rank algorithms

--- src/main/java/hackerrank/Introduction.java
+++ src/main/java/hackerrank/Introduction.java
@@ -46,4 +46,47 @@ public class Introduction {
         System.out.println(ans);
     }
 
+    /**
+     * https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+     */
+    public static void stdinAndStdout(){
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+
+    /**
+     * https://www.hackerrank.com/challenges/java-datatypes/problem
+     */
+    public static void dataTypes(){
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)
+                    System.out.println("* byte");
+                if(x >= -Math.pow(2, 15) && x <= Math.pow(2, 15) - 1)
+                    System.out.println("* short");
+                if(x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1)
+                    System.out.println("* int");
+                if(x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1)
+                    System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
 }
+
--- src/main/java/projecteuler/Main.java
+++ src/main/java/projecteuler/Main.java
@@ -1,10 +1,21 @@
 package projecteuler;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String... args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
 
+
+        Double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
-
 }
