639bb2ca85a035e864df9012efb29bca9c08da7a
Vaggelis Mavropoulos <e.mavropoulos@pamediakopes.gr>
2020-01-02 12:07:20 +0200

    Remove redundant code

--- src/main/java/projecteuler/Main.java
+++ src/main/java/projecteuler/Main.java
@@ -1,21 +1,10 @@
 package projecteuler;
 
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String... args) {
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
 
-
-        Double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
     }
 }
