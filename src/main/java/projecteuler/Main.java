2b7b284eba4903d266d4840b78d8468e1c27f59c
Vaggelis Mavropoulos <vmaurop@gmail.com>
2020-12-29 11:43:08 +0200

    Adding Algorithms - optimize

--- src/main/java/projecteuler/Main.java
+++ src/main/java/projecteuler/Main.java
@@ -1,10 +1,11 @@
 package projecteuler;
 
 
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String... args) {
-
+    public static void main(String... args) throws IOException {
+        new Euler1().largestProductInSeries();
     }
 }
